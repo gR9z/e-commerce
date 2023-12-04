bed4f822646337a066b3085878a5c47d03e2b7f2
Royer Gaétan <royergaetan90@gmail.com>
2023-12-04 18:55:48 +0100

    color, typography & header

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,15 @@
 module.exports = {
   root: true,
-  extends: ['plugin:@next/next/recommended', '@payloadcms'],
+  extends: ['plugin:@next/next/recommended', '@payloadcms', 'prettier'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'no-console': 'off',
+  },
 }
