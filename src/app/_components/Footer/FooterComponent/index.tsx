652c10c7588a67d38fe7674953f01b1b49e80326
Royer Gaétan <royergaetan90@gmail.com>
2023-12-04 20:30:10 +0100

    customize footer

---
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -0,0 +1,78 @@
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { Footer, Media } from '../../../../payload/payload-types'
+import { inclusions, noHeaderFooterUrls } from '../../../constants'
+import { Button } from '../../Button'
+import { Gutter } from '../../Gutter'
+
+import classes from './index.module.scss'
+
+const FooterComponent = ({ footer }: { footer: Footer }) => {
+  const pathname = usePathname()
+  const navItems = footer?.navItems || []
+
+  return (
+    <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
+      <Gutter>
+        <ul className={classes.inclusions}>
+          {inclusions.map((inclusion, index) => (
+            <li key={index}>
+              <Image
+                src={inclusion.icon}
+                alt={inclusion.title}
+                width={36}
+                height={36}
+                className={classes.icon}
+              />
+
+              <h5 className={classes.title}>{inclusion.title}</h5>
+              <p>{inclusion.description}</p>
+            </li>
+          ))}
+        </ul>
+      </Gutter>
+
+      <div className={classes.footer}>
+        <Gutter>
+          <div className={classes.wrap}>
+            <Link href="/">
+              <Image src="/logo-white.svg" alt="Logo" width={170} height={50} />
+            </Link>
+
+            <p>{footer.copyright}</p>
+
+            <div className={classes.socialLinks}>
+              {navItems.map(navItem => {
+                const icon = navItem?.link?.icon as Media
+
+                return (
+                  <Button
+                    key={navItem.link.label}
+                    el="link"
+                    href={navItem.link.url}
+                    newTab={true}
+                    className={classes.socialLinkItem}
+                  >
+                    <Image
+                      src={icon.url}
+                      alt={navItem.link.label}
+                      width={24}
+                      height={24}
+                      className={classes.socialIcon}
+                    />
+                  </Button>
+                )
+              })}
+            </div>
+          </div>
+        </Gutter>
+      </div>
+    </footer>
+  )
+}
+
+export default FooterComponent
