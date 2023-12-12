7b4fb74e00afdf60ca27b2cd74a0e47d6d99533a
Royer Gaétan <royergaetan90@gmail.com>
2023-12-12 16:51:11 +0100

    related products

--- src/app/_heros/Product/index.tsx
+++ src/app/_heros/Product/index.tsx
@@ -55,15 +55,15 @@ export const ProductHero: React.FC<{
 
           <p className={classes.stock}>In stock</p>
         </div>
-      </div>
 
-      <Price product={product} button={false} />
-      <div className={classes.description}>
-        <h6>Description</h6>
-        <p>{description}</p>
-      </div>
+        <Price product={product} button={false} />
+        <div className={classes.description}>
+          <h6>Description</h6>
+          <p>{description}</p>
+        </div>
 
-      <AddToCartButton product={product} className={classes.addToCartButton} />
+        <AddToCartButton product={product} className={classes.addToCartButton} />
+      </div>
     </Gutter>
   )
 }
